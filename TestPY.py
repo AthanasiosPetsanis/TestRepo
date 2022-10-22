72640cf67085a2c3e2073dfc0a80806890d18bc7
Athanasios Petsanis <athpets8@gmail.com>
2022-10-22 16:41:48 +0300

    Laughing change and *1

--- TestPY.py
+++ TestPY.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 lul = np.arange(0,10,0.01)
-cos_lul = np.cos(lul*math.pi)
+cos_lul = np.cos(lul*math.pi*1)
 plt.plot(cos_lul)
 plt.show()
-print('HAHAH')
+print('HAHAaH')
+print('HIHI')
